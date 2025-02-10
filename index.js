d9cba18dde09453b020b0039307ab5604b4c9404
RJ1205 <ryan.d@t-online.de>
2025-02-10 20:29:30 +0100

    First Commit

---
+++ index.js
@@ -0,0 +1,46 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const cityInput = document.getElementById("cityInput");
+    const button = document.getElementById("submitButton");
+    const output = document.querySelector(".output");
+    const apiKey = "c991f75ae729e4ba908cc43cbc53e6a3";
+
+    button.addEventListener("click", async (event) => {
+        event.preventDefault();
+        output.innerHTML = "";
+
+        const city = cityInput.value.trim();
+        if (city) {
+            try {
+                const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`);
+                if (!response.ok) {
+                    displayError("Please enter a proper city");
+                    return;
+                }
+
+                const data = await response.json();
+                displayWeather(data);
+                output.style.display = "block";
+
+            } catch (error) {
+                displayError("Error fetching data");
+            }
+        } else {
+            displayError("Enter a city please");
+        }
+    });
+
+    function displayWeather(data) {
+        const { name, main, weather, wind } = data;
+        const weatherInfo = `
+            <h2>Weather in ${name}</h2>
+            <p>Temperature: ${main.temp}°C</p>
+            <p>Condition: ${weather[0].description}</p>
+            <p>Wind Speed: ${wind.speed} m/s</p>
+        `;
+        output.innerHTML = weatherInfo;
+    }
+
+    function displayError(message) {
+        output.innerHTML = `<p class="error">${message}</p>`;
+    }
+});
